0cf50eb1d59aa7dd1b92b8ce0f0eb3c7b46220b1
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-02-13 21:40:52 +0530

    Create ellipse.py

---
+++ ellipse.py
@@ -0,0 +1,21 @@
+from matplotlib.image import pil_to_array
+
+a = []
+b = []
+
+from matplotlib import pyplot as plt
+import numpy as np
+for i in range(81):
+  a.append(4*np.sin(3.75*i))
+  b.append(3*np.cos(3.75*i))
+
+
+plt.scatter(a,b,label='Elipse',color='r',s=30,marker='o')
+
+plt.plot(a,b, color='b')
+plt.title('Curve')
+plt.ylabel('Range')
+plt.xlabel('Domain')
+plt.grid(True, color='k')
+
+plt.show()
