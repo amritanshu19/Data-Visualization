85cc0db82057454b490d8351cebe87a2bfb39108
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-12-17 03:08:42 +0530

    Create installed_energy_capacity_bargraph.py

---
+++ installed_energy_capacity_bargraph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+plt.bar(['Thermal','Renewable','Nuclear'],[236018.90,166362.31,6780.00], label='Mode', color='r', width = 0.5)
+plt.legend()
+#plt.xlabel('Mode wise breakup')
+plt.ylabel('Megawatt')
+plt.title(' ALL INDIA  INSTALLED CAPACITY(IN MW) OF POWER STATIONS ')
+#plt.grid(True)
+plt.show()
