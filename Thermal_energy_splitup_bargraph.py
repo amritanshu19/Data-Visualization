6179f39ac2a3a64eaf412fbd122d66f9fad5581a
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-12-17 03:10:11 +0530

    Create Thermal_energy_splitup_bargraph.py

---
+++ Thermal_energy_splitup_bargraph.py
@@ -0,0 +1,9 @@
+#Thermal energy splitup
+import matplotlib.pyplot as plt
+plt.bar(['Gas','Diesel','Coal','Lignite'],[24824.21,589.20,203985.49,6620.00], label='Mega watts')
+#plt.legend()
+plt.xlabel('Mode wise breakup')
+plt.ylabel('Megawatt')
+plt.title('Thermal energy splitup')
+#plt.grid(True)
+plt.show()
